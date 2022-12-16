ed364d74c99bce805c7388e0fdd2ded8b8dd058e
Victoria Kozhevnikova <victoria_kozhevnikova@epam.com>
2022-12-16 11:50:08 +0300

    Init project

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { task1 } from './modules/task1.js';
+
+const message = 'Hello!!!!'
+task1(message)
---
+++ src/modules/task1.ts
@@ -0,0 +1,4 @@
+export function task1(str: string): void {
+  // TODO Task 1
+  console.log(str)
+}
